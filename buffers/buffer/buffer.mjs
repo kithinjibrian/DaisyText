8990d3c7fada9b0fa999bd8d1698a43229cafe79
kithinji brian <96344063+kithinjibrian@users.noreply.github.com>
2023-07-23 00:44:02 +0300

    ceret pointer

--- buffers/buffer/buffer.mjs
+++ buffers/buffer/buffer.mjs
@@ -32,19 +32,85 @@ export default class Buffer {
   subscribe() {
     const self = this;
     Pubsub.subscribe("typing", () => {
-      const d = document.getElementById("paper");
+      const editor = document.getElementById("paper");
 
       (function () {
         self.text = {
-          innertext: d.innerText,
-          innerhtml: d.innerHTML,
-          textcontent: d.textContent,
+          innertext: editor.innerText,
+          innerhtml: editor.innerHTML,
+          textcontent: editor.textContent,
         };
       })();
 
       (function () {
+        const getTextSegments = (element) => {
+          const textSegments = [];
+          Array.from(element.childNodes).forEach((node) => {
+              switch(node.nodeType) {
+                  case Node.TEXT_NODE:
+                      textSegments.push({text: node.nodeValue, node});
+                      break;
+                      
+                  case Node.ELEMENT_NODE:
+                      textSegments.splice(textSegments.length, 0, ...(getTextSegments(node)));
+                      break;
+                      
+                  default:
+                      throw new Error(`Unexpected node type: ${node.nodeType}`);
+              }
+          });
+          return textSegments;
+        }
+        const saveCursor = (editor) => {
+          const sel = window.getSelection();
+          const textSegments = getTextSegments(editor);
+          const textContent = textSegments.map(({text}) => text).join('');
+          let anchorIndex = null;
+          let focusIndex = null;
+          let currentIndex = 0;
+          textSegments.forEach(({text, node}) => {
+              if (node === sel.anchorNode) {
+                  anchorIndex = currentIndex + sel.anchorOffset;
+              }
+              if (node === sel.focusNode) {
+                  focusIndex = currentIndex + sel.focusOffset;
+              }
+              currentIndex += text.length;
+          });
+          return {
+            anchorIndex, focusIndex
+          }
+        }
+
+        const restoreCursor = (absoluteAnchorIndex, absoluteFocusIndex) => {
+          const sel = window.getSelection();
+          const textSegments = getTextSegments(editor);
+          let anchorNode = editor;
+          let anchorIndex = 0;
+          let focusNode = editor;
+          let focusIndex = 0;
+          let currentIndex = 0;
+          textSegments.forEach(({text, node}) => {
+              const startIndexOfNode = currentIndex;
+              const endIndexOfNode = startIndexOfNode + text.length;
+              if (startIndexOfNode <= absoluteAnchorIndex && absoluteAnchorIndex <= endIndexOfNode) {
+                  anchorNode = node;
+                  anchorIndex = absoluteAnchorIndex - startIndexOfNode;
+              }
+              if (startIndexOfNode <= absoluteFocusIndex && absoluteFocusIndex <= endIndexOfNode) {
+                  focusNode = node;
+                  focusIndex = absoluteFocusIndex - startIndexOfNode;
+              }
+              currentIndex += text.length;
+          });
+          
+          sel.setBaseAndExtent(anchorNode,anchorIndex,focusNode,focusIndex);
+        }
+
+        let {anchorIndex, focusIndex} = saveCursor();
         const h = Prism.highlight(self.text.textcontent, Prism.languages.javascript, "javascript")
         d.innerHTML = h;
+        restoreCursor(anchorIndex,focusIndex)
       })();
     });
   }
